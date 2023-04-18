bcc9bfb2c0adda96bbd58316b28a183ed08a3cd5
b21946663 <56929042+b21946663@users.noreply.github.com>
2023-04-18 15:43:35 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+    const CLIENT_ID = "239ae9d556f74055af35304e818d9ae6"
+    const REDIRECT_URI = "http://localhost:3000"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/api/token"
+    const RESPONSE_TYPE = "token"
+
+    // const [token, setToken] = useState("")
+    // const [searchKey, setSearchKey] = useState("")
+    // const [artists, setArtists] = useState([])
+
+    // function test() {
+    //     /* eslint-disable no-undef */
+    //     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    //         const activeTabId = tabs[0].id
+    //         chrome.scripting.executeScript(
+    //             {
+    //                 target: {tabId: activeTabId},
+    //                 function: () => alert("React Chrome Extension Alert"),
+    //             })
+    //     })
+    // }
+
+    return (
+        <div className="App">
+            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+                Login to Spotify
+            </a>
+
+        </div>
+    );
+}
+
+export default App;
