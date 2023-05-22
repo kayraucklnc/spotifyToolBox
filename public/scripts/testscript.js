519750bf55015eb17b39a04fb3f7504da9d59727
Enes GÖKCE <56717764+enes-gokce@users.noreply.github.com>
2023-05-22 22:56:31 +0300

    Token structure for spotify api usage and download playlist as json via spotify api

--- public/scripts/testscript.js
+++ public/scripts/testscript.js
@@ -12,51 +12,6 @@ document.body.appendChild(newDiv);
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'downloadJson') {
 
-        let playListId = request.url.split("/")[4];
-
-            /*let allTitles = document.querySelectorAll(".t_yrXoUO3qGsJS4Y6iXX div");
-            let allArtists = document.querySelectorAll(".rq2VQ5mb9SDAFWbBIUIn");
-            let allAlbums = document.querySelectorAll(".bfQ2S9bMXr_kJjqEfcwA a");
-            let allDurations = document.querySelectorAll(".HcMOFLaukKJdK5LfdHh0 div");
-
-            // Create an empty array to hold data
-            let jsonData = [];
-
-            // Loop through data and push to the array
-            for (let i = 0; i < allTitles.length; i++) {
-                let temp = [];
-                let artists = allArtists[i].getElementsByTagName("a");
-                Array.from(artists).forEach(element => {
-                    temp.push(element.innerHTML);
-                });
-                let item = {
-                    title: allTitles[i].innerHTML,
-                    artists: temp,
-                    album: allAlbums[i].innerHTML,
-                    duration: allDurations[i+1].innerHTML,
-                };
-                jsonData.push(item);
-            }
-
-            // Convert array to JSON string
-            let jsonString = JSON.stringify(jsonData);
-
-            // Create a Blob with the JSON string
-            let blob = new Blob([jsonString], {type: "application/json"});
-
-            // Create a download link
-            let downloadLink = document.createElement("a");
-            downloadLink.href = URL.createObjectURL(blob);
-            downloadLink.download = "data.json";
-            downloadLink.style.display = "none";
-            document.body.appendChild(downloadLink);
-
-            // Trigger download
-            downloadLink.click();
-
-            // Clean up
-            document.body.removeChild(downloadLink); */
-
         const clientID = '95a834e74870414da4c4fe63d3153de6';
         const clientSecret = 'ee5e6d4f94a04f838119c28e3aabd16a';
         let accessToken = 'deneme';
