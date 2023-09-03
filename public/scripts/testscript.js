bf3a660d69d0d777a7b225ac062f452d4e4bb485
Enes GÖKCE <56717764+enes-gokce@users.noreply.github.com>
2023-09-03 14:30:04 +0300

    Implementation of download playlist as json file from SpotifyAPI completed.

--- public/scripts/testscript.js
+++ public/scripts/testscript.js
@@ -1,92 +1,92 @@
 // Content script code
 
-// Create a new div element
-const newDiv = document.createElement("div");
+/**
+ * Retrieves an access token from Spotify's API using client credentials.
+ *
+ * @returns {string} The access token.
+ */
+async function getAccessToken() {
 
-// Set the content of the div element
-newDiv.innerHTML = "Hello, world!";
-newDiv.className = "anan"
+    const clientID = '95a834e74870414da4c4fe63d3153de6';
+    const clientSecret = 'ee5e6d4f94a04f838119c28e3aabd16a';
+    let accessToken = 'deneme';
 
-// Add the new div element to the Spotify web player's body element
-document.body.appendChild(newDiv);
+    const data = {
+        grant_type: 'client_credentials',
+        client_id: clientID,
+        client_secret: clientSecret
+    };
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(data)
+    });
+    const responseData = await response.json();
+    return responseData.access_token;
+}
+
+// Retrieves playlist from SpotifyAPI via accessToken and playlistID.
+async function getPlaylist(accessToken, playlistID) {
+    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    return response.json();
+}
+
+function processTracks(tracks) {
+    return tracks.map(track => ({
+        title: track.track.name,
+        artists: track.track.artists.map(artist => artist.name),
+        album: track.track.album.name,
+        duration: `${Math.floor(track.track.duration_ms / 1000 / 60)}:${Math.floor(track.track.duration_ms / 1000 % 60)}`
+    }));
+}
+
+function downloadAsJSONFile(array){
+    // Convert array to JSON string
+    let jsonString = JSON.stringify(array);
+
+    // Create a Blob with the JSON string
+    let blob = new Blob([jsonString], {type: "application/json"});
+
+    // Create a download link
+    let downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = "data.json";
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    // Trigger download
+    downloadLink.click();
+
+    // Clean up
+    document.body.removeChild(downloadLink);
+}
+
+/**
+ * Downloads a Spotify playlist's track information via the Spotify API and saves it as a JSON file.
+ *
+ * @param {Request} request - The incoming HTTP request containing the playlist URL.
+ */
+async function downloadPlaylistViaAPI(request) {
+    try {
+        const accessToken = await getAccessToken();
+        const playlistID = request.url.split("/")[4];
+        const playlistData = await getPlaylist(accessToken, playlistID);
+        const allMusicInfos = processTracks(playlistData.tracks.items);
+        downloadAsJSONFile(allMusicInfos);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'downloadJson') {
-
-        const clientID = '95a834e74870414da4c4fe63d3153de6';
-        const clientSecret = 'ee5e6d4f94a04f838119c28e3aabd16a';
-        let accessToken = 'deneme';
-
-        const data = {
-            grant_type: 'client_credentials',
-            client_id: clientID,
-            client_secret: clientSecret
-        };
-
-        fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams(data)
-        })
-            .then(response => response.json())
-            .then(data => {
-                accessToken = data.access_token;
-                // Use the access token for authenticated requests
-                const playlistID = request.url.split("/")[4];
-
-                fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        // Handle the playlist data
-                        const allTracks = data.tracks.items;
-                        let allMusicInfos = [];
-
-                        for(let i = 0; i<allTracks.length; i++){
-                            let track = allTracks[i].track;
-                            let musicInfo = {
-                                title: track.name,
-                                artists: track.artists.map(artist => artist.name),
-                                album: track.album.name,
-                                duration: Math.floor(track.duration_ms/1000 / 60) + ":" + Math.floor(track.duration_ms/1000 % 60)
-                            }
-                            allMusicInfos.push(musicInfo)
-                        }
-                        console.log(allMusicInfos);
-                        downloadAsJSONFile(allMusicInfos)
-                    })
-                    .catch(error => {
-                        console.error('Error:', error);
-                    });
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-
-
-        function downloadAsJSONFile(array){
-            // Convert array to JSON string
-            let jsonString = JSON.stringify(array);
-
-            // Create a Blob with the JSON string
-            let blob = new Blob([jsonString], {type: "application/json"});
-
-            // Create a download link
-            let downloadLink = document.createElement("a");
-            downloadLink.href = URL.createObjectURL(blob);
-            downloadLink.download = "data.json";
-            downloadLink.style.display = "none";
-            document.body.appendChild(downloadLink);
-
-            // Trigger download
-            downloadLink.click();
-
-            // Clean up
-            document.body.removeChild(downloadLink);
-        }
+        downloadPlaylistViaAPI(request);
     }
 });
