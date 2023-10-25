31ed05fb6f9e220cefa1c0e9efa3e975d0f2e3bb
gokcuenal16 <gokceunal6@gmail.com>
2023-10-25 20:46:13 +0300

    youtube-logo added to dom

--- public/scripts/youtubeAdder.js
+++ public/scripts/youtubeAdder.js
@@ -2,8 +2,9 @@
 const SELECTOR_OF_LIST = '#main > div > div.ZQftYELq0aOsg6tPbVbV > div.jEMA2gVoLgPQqAFrPhFw > div.main-view-container > div.os-host.os-host-foreign.os-theme-spotify.os-host-resize-disabled.os-host-scrollbar-horizontal-hidden.main-view-container__scroll-node.os-host-transition.os-host-overflow.os-host-overflow-y > div.os-padding > div > div > div.main-view-container__scroll-node-child > main > div.GlueDropTarget.GlueDropTarget--tracks.GlueDropTarget--local-tracks.GlueDropTarget--episodes.GlueDropTarget--albums > section > div.rezqw3Q4OEPB1m4rmwfw > div.contentSpacing > div.ShMHCGsT93epRGdxJp2w.Ss6hr6HYpN4wjHJ9GHmi > div.JUa6JJNj7R_Y3i4P8YUX > div:nth-child(2)'
 
 function getMusicListHolder() {
-    return  document.querySelector(SELECTOR_OF_LIST);
+    return document.querySelector(SELECTOR_OF_LIST);
 }
+
 function addSubElement(targetElement) {
     childElements = targetElement.children
     for (let i = 0; i < childElements.length; i++) {
@@ -13,10 +14,9 @@ function addSubElement(targetElement) {
 }
 
 
-
 setTimeout(start, 2000);
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === 'triggerFunction') {
         // Call the desired function in the content script
         setTimeout(start, 1000);
@@ -24,7 +24,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 });
 
 
-function start(){
+function start() {
     let targetElement = getMusicListHolder();
     addSubElement(targetElement);
     watchContentChanges(targetElement)
@@ -66,17 +66,17 @@ function addButton(elem) {
     console.log(songName);
     nameArr = songName.split()
     let songNameElement = elem.children[0].children[1]
-    addLink(songNameElement, "Youtube Link", "https://www.youtube.com/results?search_query=" + nameArr.join("+"))
+    addLink(songNameElement, "https://www.youtube.com/results?search_query=" + nameArr.join("+"))
 }
 
-function addLink(targetElement, text, href) {
-    const linkElement = document.createElement('a');
+function addLink(targetElement, href) {
+    const linkElement = document.createElement('div');
     linkElement.draggable = true;
-    linkElement.classList.add("standalone-ellipsis-one-line", "ytbutton");
+    linkElement.classList.add("standalone-ellipsis-one-line", "ytbutton", "youtube-logo");
     linkElement.setAttribute('dir', 'auto');
     linkElement.href = href;
     linkElement.tabIndex = -1;
-    linkElement.textContent = text;
+
 
     targetElement.appendChild(linkElement);
 }
