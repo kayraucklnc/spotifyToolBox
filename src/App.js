42ab4562e93350f0694a9930a36e08bb23cb39d8
Enes GÖKCE <56717764+enes-gokce@users.noreply.github.com>
2023-11-04 15:12:57 +0300

    getting song id's selected songs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+/*global chrome*/
+
 import './style/index.css';
 import Spotify from "./spotify";
 import SpotifyButton from "./components/SpotifyButton";
 
+const playlistToJson = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const currentUrl = tabs[0].url;
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'downloadJson', url: currentUrl });
+    });
+}
+
+const deleteSongs = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const currentUrl = tabs[0].url;
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'deleteSongs', url: currentUrl });
+    });
+}
+
 function App() {
     return (
         <div className="App">
-            <SpotifyButton text="Play on Spotify" onClick={() => {console.log("anan")}}/>
+            <SpotifyButton text="Download playlist" onClick={playlistToJson}/>
+            <SpotifyButton text="Delete songs" onClick={deleteSongs}/>
             <Spotify/>
         </div>
     );
